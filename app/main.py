7473235b4a58ca81405c50805c5d5293a0653d7b
Vitalii Mykytenko <vitalii.mykytenko@gmail.com>
2024-07-29 00:54:53 +0300

    class constructor __init__()

--- app/main.py
+++ app/main.py
@@ -2,5 +2,7 @@ from __future__ import annotations
 
 
 class Vector:
-    # write your code here
-    pass
+
+    def __init__(self, abscissa: int | float, ordinate: int | float) -> None:
+        self.x = round(abscissa, 2)
+        self.y = round(ordinate, 2)

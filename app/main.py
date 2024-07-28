2287086f5e1629006b6bb0ba098501390513a3b3
Vitalii Mykytenko <vitalii.mykytenko@gmail.com>
2024-07-29 01:03:56 +0300

    class (magic) method __add__()

--- app/main.py
+++ app/main.py
@@ -6,3 +6,7 @@ class Vector:
     def __init__(self, abscissa: int | float, ordinate: int | float) -> None:
         self.x = round(abscissa, 2)
         self.y = round(ordinate, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y)
